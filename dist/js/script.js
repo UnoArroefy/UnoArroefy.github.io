b2ed7a4dd062e7cf776eb8d1fd0704154a816161
UnoArroefy <numroua@gmail.com>
2023-08-20 09:27:43 +0700

    reveal content on scroll

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,6 +14,19 @@ window.addEventListener("scroll", () => {
   menuNav.classList.remove("menu-active");
 });
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden")
+hiddenElements.forEach((el) => observer.observe(el))
 
 var options = {
   strings: ['Numero Uno Arroefy', 'yqroo', 'UnoArroefy'],
